4e9fb9a838c657e166753ca49e61fb606fab4cfb
downloadibrahim6-stack <downloadibrahim6@gmail.com>
2026-09-18 13:19:59 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -802,9 +802,11 @@ buffer = BytesIO()
 img.save(buffer, format="PNG")
 buffer.seek(0)
 await call.message.answer_photo(photo=buffer, caption="यहाँ आपका UPI QR कोड है।")
-return{"status": "error", "message": "Failed to parse response"}
+return {"status": "error", "message": "Failed to parse response"}
+
 else:
-                    return {"status": "error", "message": f"HTTP Error: {resp.status}"}
+    return {"status": "error", "message": f"HTTP Error: {resp.status}"}
+    
         except Exception as e:
             logger.error(f"FamPay API Error: {e}")
             return {"status": "error", "message": str(e)}
